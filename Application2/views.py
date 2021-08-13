a61140da3e90b668a5aa3a317eba2519ba44a836
NIRUa <bojjaniranjanreddy@gmail.com>
2021-08-13 14:21:51 +0530

    first commit

---
+++ Application2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def Application2_Mani(request):
+    return HttpResponse('Mani is an innocent')
